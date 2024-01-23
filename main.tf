3addc5d947487b4fbf5724d6b9006eb5a61ebb56
PabloEscales <pabloescales@gmail.com>
2024-01-23 15:34:58 +0100

    role aim added

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region  = var.region
-  profile = "poel"
+  profile = "LearningDev"
 }
 
 resource "tls_private_key" "rsa_4096" {
@@ -51,6 +51,46 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "internet_gateway"
   }
+}
+
+resource "aws_iam_policy" "policy_admin" {
+  name = "policy-618033"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["*"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role" "test_role" {
+  name = "poel-terraform-ec2"
+  managed_policy_arns = [ aws_iam_policy.policy_admin ]
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "test_profile"
+  role = aws_iam_role.test_role.name
 }
 
 resource "aws_instance" "ec2" {
